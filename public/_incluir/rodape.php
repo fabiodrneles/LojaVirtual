4be532de9d65f61648f2f0f1fff64fbf76b8df7a
fabiodrneles <fabiodrneles@gmail.com>
2023-05-10 15:33:02 -0300

    editando o rodapé da pagina

--- public/_incluir/rodape.php
+++ public/_incluir/rodape.php
@@ -1,5 +1,5 @@
 <footer>
     <div id="footer_central">
-    <p>ANDES é uma empresa fictícia, usada para o curso PHP Integração com MySQL.</p>
+    <p>ANDES é uma empresa fictícia, usada para o módulo de PHP da NewTab Academy.</p>
     </div>
 </footer>
