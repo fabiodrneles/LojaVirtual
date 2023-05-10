a85f82af3efe7affe88db761b3e023ca965b23b2
fabiodrneles <fabiodrneles@gmail.com>
2023-05-10 15:36:38 -0300

    editando função de formatação de moeda

--- public/_incluir/funcoes.php
+++ public/_incluir/funcoes.php
@@ -1,3 +1,4 @@
+<!-- esta função serve para formatar o estilo de moeda dos produtos no formato brasileiro. -->
 <?php
     function real_format($valor) {
         $valor  = number_format($valor,2,",",".");
